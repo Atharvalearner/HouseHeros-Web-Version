86a0b59e33ee486940de68a34a329515debd7966
Atharvalearner <atharvabanchhode09@gmail.com>
2025-10-13 10:55:34 +0530

    HouseHeros Backend Setup

---
+++ src/main/java/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import models.User;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
